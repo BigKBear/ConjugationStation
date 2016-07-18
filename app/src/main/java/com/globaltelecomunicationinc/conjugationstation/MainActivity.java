3c39bf71ae1614d3df6655c12f50461b3ff13449
Kyle St-Hill <kylesthill77@gmail.com>
2016-07-18 16:58:14 -0400

    set orentation to be vertical

--- app/src/main/java/com/globaltelecomunicationinc/conjugationstation/MainActivity.java
+++ app/src/main/java/com/globaltelecomunicationinc/conjugationstation/MainActivity.java
@@ -1,26 +1,29 @@
 package com.globaltelecomunicationinc.conjugationstation;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
-    Button btnStart, btnQuit;
+    ImageButton btnStart, btnQuit;
 
     public void initViews() {
-        btnStart = (Button) findViewById(R.id.btnStart);
-        btnQuit = (Button) findViewById(R.id.btnQuit);
+        btnStart = (ImageButton) findViewById(R.id.btnStart);
+        btnQuit = (ImageButton) findViewById(R.id.btnQuit);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
         initViews();
         setListeners();
